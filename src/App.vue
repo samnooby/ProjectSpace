1ee4e6664f16a2a9be26a109284f9c493b74cd7c
Sam <samjnewby@hotmail.com>
2019-02-25 14:23:52 -0500

    Added Veutify to project

--- src/App.vue
+++ src/App.vue
@@ -22,4 +22,6 @@ export default {
 }
 </script>
 
-<style></style>
+<style>
+@import '/assets/VuetifyIcons.css';
+</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import Vue from 'vue'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
 import App from './App.vue'
 import router from './router'
 import store from './store'
@@ -6,6 +8,7 @@ import SpaceForceLogo from '@/components/SpaceForceLogo.vue'
 
 Vue.config.productionTip = false
 
+Vue.use(Vuetify)
 Vue.component('SpaceForceLogo', SpaceForceLogo)
 
 new Vue({
