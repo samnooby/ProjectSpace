a60c6198446d51d4dc9a4545107d373b9686797c
Sam <samjnewby@hotmail.com>
2019-02-25 13:46:38 -0500

    Added base components to Header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,11 +1,17 @@
 <template>
-  <div>
+  <div class="Header">
+    <SpaceForceLogo class="logo"></SpaceForceLogo>
     <h1>This is a header</h1>
   </div>
 </template>
 
 <script>
-export default {}
+import SpaceForceLogo from '@/components/SpaceForceLogo.vue'
+export default {
+  components: {
+    SpaceForceLogo
+  }
+}
 </script>
 
 <style scoped></style>
---
+++ src/components/SpaceForceLogo.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="space-logo">
+    <h1>This is the spaceforce logo</h1>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+.space-logo {
+  max-width: 100px;
+  max-height: 100px;
+}
+</style>
